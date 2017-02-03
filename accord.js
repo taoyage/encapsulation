11177c7fb596c68624f37f35bc729b67f1284429
taoyage <tougakaku@gmail.com>
2017-02-04 00:44:41 +0900

    原型中添加需要链式访问的方法&&属性

--- accord.js
+++ accord.js
@@ -1,42 +1,32 @@
 /*
  * @Author: taoyage
- * @FileName: accord.js 						   
- * @Date:   2017-01-27 01:28:00 						   
- * @Last Modified by:   taoyage 	   
- * @Last Modified time: 2017-02-02 23:47:00 	   
+ * @FileName: accord.js                            
+ * @Date:   2017-01-27 01:28:00                            
+ * @Last Modified by:   taoyage        
+ * @Last Modified time: 2017-02-04 00:43:21        
  */
 
-
-/**
- * @description
- * 
- * 将代码包裹在立即执行的匿名函数当中，以避免对全局环境造成污染
- * 通过传入 window 对象，可以使 window 对象变为局部变量，
- * 这样当在 jQuery 代码块中访问 window 对象时，不需要将作用域链回退到顶
- * 层作用域，从而可以更快地访问 window 对象
- * 
- * @param  {[Object]} 	 window 
- * @return {[type]}     [description]
- */
 (function(w) {
     'use strict';
 
     var
         arr = [],
+        slice = arr.slice,
         push = arr.push;
 
-    /**
-     * @discription 创建Accord构造函数
-     * @param {[String]} selector 				  [选择器]
-     * @param {[Object]} context  				  [HTMLElement 选择器上下文]
-     * @return new Accord.init(selector,context)  [方便用户使用Accord()时,无需再次进行实例化操作]
-     */
-    var Accord = function(selector, context) {
-        return new Accord.fn.init(selector, context);
-    };
+
+        /**
+         * @discription 创建Accord构造函数
+         * @param {[String]} selector                 [选择器]
+         * @param {[Object]} context                  [HTMLElement 选择器上下文]
+         * @return new Accord.init(selector,context)  [方便用户使用Accord()时,无需再次进行实例化操作]
+         */
+        var Accord = function(selector, context) {
+            return new Accord.fn.init(selector, context);
+        };
 
     /**
-     * [创建原型对象,并添加一些默认的属性与方法]
+     * [创建原型对象,并添加一些默认的属性与参加链式访问的方法]
      * @type {Function}
      */
     Accord.fn = Accord.prototype = {
@@ -51,7 +41,7 @@
 
         //将伪数组转换成真正的数组
         toArray: function() {
-            return arr.slice.call(this);
+            return slice.call(this);
         },
 
         //通过下标获取dom元素
@@ -60,13 +50,13 @@
                 (num < 0 ? this[this.length + num] : this[num]);
         },
 
+        //创建一个新的空Accord对象,然后把DOM元素集合添加到新对象中,并将当前对象保存在prevObject中
         pushStack: function(elems) {
-
+            console.log(elems)
+            //通过merge方法合并对象
             var ret = Accord.merge(this.constructor(), elems)
-
             ret.prevObject = this;
             ret.context = this.context;
-
             return ret;
         },
 
@@ -75,31 +65,44 @@
             return Accord.each(this, callback, args);
         },
 
-
-        ready: function() {},
-
-
-        eq: function(num) {
-            var len = this.length;
-
+        //通过下标获取元素,并将元素添加到新对象中,返回该对象
+        eq: function(i) {
+            var
+                len = this.length,
+                j = +i + (i < 0 ? len : 0);
+            return this.pushStack(j >= 0 && j < len ? [this[j]] : []);
         },
 
-        first: function() {},
-        last: function() {},
+        //获取第一个元素
+        first: function() {
+            return this.eq(0);
+        },
+
+        //获取最后一个
+        last: function() {
+            return this.eq(-1);
+        },
+
+        //通过begin,end截取元素,并添加到新对象中
         slice: function() {
-            return 1;
+            return this.pushStack(slice.apply(this, arguments));
         },
+
         map: function() {},
-        end: function() {},
+
+        //结束当前链条中最近的筛选操作，并将匹配元素集合还原为之前的状态
+        end: function() {
+            return this.prevObject || this.constructor();
+        },
     };
 
     /**
      * @discription 
      * 用来解析参数selector和context的类型并执行相应的查找
      * 
-     * @param  {[String]} selector 	[选择器]
-     * @param  {[Object]} context  	[选择器上下文]
-     * @return {[type]}          	[description]
+     * @param  {[String]} selector  [选择器]
+     * @param  {[Object]} context   [选择器上下文]
+     * @return {[type]}             [description]
      */
     var init = Accord.fn.init = function(selector, context) {
         var
@@ -130,7 +133,7 @@
             return this;
         }
 
-        //	
+        //  
         if (typeof selector === "string") {
             this.context = context || document;
             nodeList = this.context.querySelectorAll(selector);
@@ -144,6 +147,7 @@
     };
 
     init.prototype = Accord.fn;
+
     /**
      * [extend 对Accord或Accord原型进行方法的扩展,也可指定对某个对象进行扩展]
      * @return {[object]} [返回当前对象]
@@ -171,46 +175,116 @@
         return target;
     };
 
-    /*基本数据类型判断*/
+
     Accord.extend({
+
+        /**
+         * @name Accord.isArray
+         * @kind function
+         *
+         * @description
+         * Determines if a reference is an `Array`. Alias of Array.isArray.
+         *
+         * @param {*} value Reference to check.
+         * @returns {Boolean} True if `value` is an `Array`.
+         */
         isArray: Array.isArray,
+
+
+        /**
+         * @name Accord.isUndefined
+         * @kind function
+         *
+         * @description
+         * Determines if a reference is undefined
+         * 
+         * @param  {[*]}  value Reference to check
+         * @return {Boolean}   True if `value` is a undefined
+         */
         isUndefined: function(value) {
             return typeof value === 'undefined';
         },
+
+
+        /**
+         * @name Accord.isDefined
+         * @kind function
+         *
+         * @description
+         * Determines if a reference is defined
+         * 
+         * @param  {[*]}  value Reference to check
+         * @return {Boolean}       True if `value` is a defined
+         */
         isDefined: function(value) {
             return typeof value !== 'undefined';
         },
-        isObject: function(value) {
-            return typeof value === 'object';
-        },
-        isBlankObject: function(value) {
 
+
+        /**
+         * @name Accord.isObject
+         * @kind function
+         *
+         * @description 
+         * Determines if a reference is an `object`.Unlike `typeof` in JavaScript, 
+         * null's are not considered to be objects. Note that JavaScript arrays are objects.
+         * 
+         * @param  {[*]}  value Reference to check 
+         * @return {Boolean}     True if `value` is a object
+         */
+        isObject: function(value) {
+            return value != null && typeof value === 'object';
         },
+
+
+        /**
+         * @name Accord.isString
+         * @kind function
+         *
+         * @description
+         * Determines if a refermence is a `String`
+         * 
+         * @param  {[*]}  value Refermence to check.
+         * @return {Boolean}  True if value is a `String`
+         */
         isString: function(value) {
             console.log(value);
             return typeof value === 'string';
         },
+
         isNumber: function(value) {
             return typeof value === 'number';
         },
+
         isDate: function(value) {
             return toString.call(value) === '[object Date]';
         },
+
         isFunction: function(value) {
             return typeof value === 'function';
         },
+
         isRegExp: function(value) {
             return toString.call(value) === '[object RegExp]';
         },
+
         isFile: function(obj) {
             return toString.call(value) === '[object File]';
         },
+
         isFormData: function(obj) {
             return toString.call(value) === '[object FormData]'
         },
+
         isBoolean: function(value) {
             return typeof value === 'boolean';
         },
+
+        trim: function(value) {
+            return typeof value === 'String' ? value.trim() : value
+        },
+
+        //遍历
         each: function(obj, callback, args) {
             var
                 name,
@@ -227,6 +301,21 @@
                     callback.call(obj[i], obj[i], i, obj);
                 }
             }
+        },
+
+        //合并对象
+        merge: function(first, second) {
+            var
+                len = +second.length,
+                j = 0,
+                i = first.length;
+
+            for (; j < len; j++) {
+                first[i++] = second[j];
+            }
+            first.lenght = i;
+
+            return first;
         }
     });
 
